f9cec2ada8ec517b0f43396049ef32ac30e3b3d6
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2024-08-05 20:14:31 -0300

    First Lesson

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PauloQuagliatto/react-go-2024.git
+
+go 1.22.0
+
+require github.com/joho/godotenv v1.5.1
---
+++ internal/store/pgstore/migrations/002_create_rooms_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS messages (
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  "room_id" uuid NOT NULL,
+  "message" VARCHAR(255) NOT NULL,
+  "reaction_count" BIG_INT NOT NULL DEFAULT 0,
+  "answered" BOOL NOT NULL DEFAULT false,
+
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS messages;
